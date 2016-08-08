f7e3f5327b493806350533a45a804aafed0e0997
powolnymarcel <powolnymarcel@gmail.com>
2016-08-09 00:13:42 +0200

    skeleton

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,10 +1,90 @@
 @extends('layouts.master')
+
 @section('title')
-    Le panier d'achat laravel
-    @endsection
+    Laravel Shopping Cart
+@endsection
 
 @section('content')
-
-<h1>Fonctionne</h1>
-
-    @endsection
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://learninglaravel.net/img/laravel5cookbook.png" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Laravel cookbook</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">€12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Ajouter au panier</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://learninglaravel.net/img/laravel5cookbook.png" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Laravel cookbook</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">€12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Ajouter au panier</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://learninglaravel.net/img/laravel5cookbook.png" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Laravel cookbook</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">€12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Ajouter au panier</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://learninglaravel.net/img/laravel5cookbook.png" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Laravel cookbook</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">€12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Ajouter au panier</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://learninglaravel.net/img/laravel5cookbook.png" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Laravel cookbook</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">€12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Ajouter au panier</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://learninglaravel.net/img/laravel5cookbook.png" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Laravel cookbook</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">€12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Ajouter au panier</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
